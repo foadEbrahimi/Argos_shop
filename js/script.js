9e04731b7392184f76e5ca980eee6b4dbe9ec787
Foad <fwad40901@gmail.com>
2023-10-06 14:34:58 +0330

    Add Offers Section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let navbarAside = document.querySelector(".navbar-Aside");
 shopList.addEventListener("click", () =>
   navbarAside.classList.toggle("show-Aside")
 );
+const swiper = new swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
