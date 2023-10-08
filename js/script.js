5b66179f727a2823f4f6fb9d13d5f802a9e69a24
Foad <fwad40901@gmail.com>
2023-10-08 16:37:18 +0330

    Add Footer Section

--- js/script.js
+++ js/script.js
@@ -4,11 +4,3 @@ let navbarAside = document.querySelector(".navbar-Aside");
 shopList.addEventListener("click", () =>
   navbarAside.classList.toggle("show-Aside")
 );
-const swiper = new swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
