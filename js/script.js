016593f5e2c8861b5273495cd42ee710a87dbac5
Foad <fwad40901@gmail.com>
2023-10-08 17:13:55 +0330

    Add Navbar-Aside Section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let shopList = document.querySelector(".shop-item");
+let shopLinks = document.querySelector(".shop-links");
 let navbarAside = document.querySelector(".navbar-Aside");
 
-shopList.addEventListener("click", () =>
-  navbarAside.classList.toggle("show-Aside")
-);
+shopList.addEventListener("click", () => {
+  navbarAside.classList.toggle("show-Aside");
+});
